a487eb250e6aaa6f71fff1d94360a2eabf1e665c
ZappacenereAlessio <Alessiozappacenere06@outlook.it>
2022-03-29 06:49:59 +0200

    Data Manager push

--- src/datamanager/JSONReader.java
+++ src/datamanager/JSONReader.java
@@ -1,9 +1,10 @@
+package datamanager;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jsonmanager;
+
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
--- src/datamanager/Libro.java
+++ src/datamanager/Libro.java
@@ -1,4 +1,4 @@
-package jsonmanager;
+package datamanager;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
